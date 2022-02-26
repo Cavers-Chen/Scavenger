1e040f7b975b40df46ca50054f6b92b2d6d842c9
Zhouyuan-Chen <867442167@qq.com>
2022-02-26 12:55:49 +0800

    start project

---
+++ Assets/Scavenger/Scripts/GameManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float levelStartDelay = 2f;
+    public float turnDelay = .1f;
+    public static GameManager instance = null;
+    public BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playerTurn = true;
+
+    private Text levelText;
+    private GameObject levelImage;
+    private int level = 1;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+    private void OnLevelWasLoaded(int level)
+    {
+        this.level++;
+        InitGame();
+    }
+
+    void Awake(){
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        enemies = new List<Enemy>();
+        InitGame();
+    }
+
+    void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+        doingSetup = false;
+    }
+
+    void InitGame(){
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("levelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        boardScript.SetupScene(level);
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+    void Update(){
+        if (playerTurn || enemiesMoving || doingSetup) 
+            return;
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if (enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].moveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playerTurn = true;
+        enemiesMoving = false;
+    }
+}
